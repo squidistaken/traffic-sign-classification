cf21e05010a672e36df20f2189254dc4c2e64125
Richard <richitdd@gmail.com>
2025-10-16 14:44:03 +0200

    ReLU activation function forward + backward

--- nn/activations.py
+++ nn/activations.py
@@ -1,1 +1,18 @@
 # ReLU, LeakyReLU, Tanh, Sigmoid, Softmax
+
+from typing import Tuple
+
+
+def relu_forward(x: float) -> Tuple[float, float]:
+    """
+    Rectified Linear Unit activation function.
+    Returns a tuple (output, grad) where output is the ReLU of x and grad is the gradient of ReLU at x.
+    """
+    return max(0.0, x), 1.0 if x > 0 else 0.0
+
+
+def relu_backward(dout: float, grad: float) -> float:
+    """
+    Backward pass for ReLU activation function.
+    """
+    return dout * grad
